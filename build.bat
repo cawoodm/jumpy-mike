ff0c457f7dbf3d552ee7ee6c3a8deb47150b434f
Marc Cawood <cawoodm@gmail.com>
2017-08-17 20:40:48 +0200

    Release

--- build.bat
+++ build.bat
@@ -24,9 +24,10 @@ TYPE game.js >> app.js
 COPY /Y app.js release\
 
 COPY /Y .\release\*.* "C:\Users\marc\Google Drive\Work\cawoodm.github.io\jumpy-mike"
-CD ..\..\JavaScript\games\trex
+CD ..\..\cawoodm.github.io\jumpy-mike
 git add .
 git commit -m "Release"
 git push origin master
 
+::CD ..\..\JavaScript\games\trex
 CD %dp0
