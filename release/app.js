306be9be54d5f9cf8fc80ee5c108a161b7b5a0f2
Marc Cawood <cawoodm@gmail.com>
2017-09-08 07:33:11 +0200

    Build 08.09.2017  7:33:11.69

--- release/app.js
+++ release/app.js
@@ -1,30 +1,30 @@
 !function(t,e){"function"==typeof define&&define.amd?define(["exports"],e):e("object"==typeof exports&&"string"!=typeof exports.nodeName?exports:t.TinyMusic={})}(this,function(t){function e(t){var s=t.split(c);this.frequency=e.getFrequency(s[0])||0,this.duration=e.getDuration(s[1])||0}function s(t,e,s){this.ac=t||new AudioContext,this.createFxNodes(),this.tempo=e||120,this.loop=!0,this.smoothing=0,this.staccato=0,this.notes=[],this.push.apply(this,s||[])}var i="B#-C|C#-Db|D|D#-Eb|E-Fb|E#-F|F#-Gb|G|G#-Ab|A|A#-Bb|B-Cb",o=440*Math.pow(Math.pow(2,1/12),-9),n=/^[0-9.]+$/,r=4,c=/\s+/,h=/(\d+)/,a={};i.split("|").forEach(function(t,e){t.split("-").forEach(function(t){a[t]=e})}),e.getFrequency=function(t){var e=t.split(h),s=a[e[0]],i=(e[1]||r)-r,n=o*Math.pow(Math.pow(2,1/12),s);return n*Math.pow(2,i)},e.getDuration=function(t){return n.test(t)?parseFloat(t):t.toLowerCase().split("").reduce(function(t,e){return t+("w"===e?4:"h"===e?2:"q"===e?1:"e"===e?.5:"s"===e?.25:0)},0)},s.prototype.createFxNodes=function(){var t=[["bass",100],["mid",1e3],["treble",2500]],e=this.gain=this.ac.createGain();return t.forEach(function(t,s){s=this[t[0]]=this.ac.createBiquadFilter(),s.type="peaking",s.frequency.value=t[1],e.connect(e=s)}.bind(this)),e.connect(this.ac.destination),this},s.prototype.push=function(){return Array.prototype.forEach.call(arguments,function(t){this.notes.push(t instanceof e?t:new e(t))}.bind(this)),this},s.prototype.createCustomWave=function(t,e){e||(e=t),this.waveType="custom",this.customWave=[new Float32Array(t),new Float32Array(e)]},s.prototype.createOscillator=function(){return this.stop(),this.osc=this.ac.createOscillator(),this.customWave?this.osc.setPeriodicWave(this.ac.createPeriodicWave.apply(this.ac,this.customWave)):this.osc.type=this.waveType||"square",this.osc.connect(this.gain),this},s.prototype.scheduleNote=function(t,e){var s=60/this.tempo*this.notes[t].duration,i=s*(1-(this.staccato||0));return this.setFrequency(this.notes[t].frequency,e),this.smoothing&&this.notes[t].frequency&&this.slide(t,e,i),this.setFrequency(0,e+i),e+s},s.prototype.getNextNote=function(t){return this.notes[t<this.notes.length-1?t+1:0]},s.prototype.getSlideStartDelay=function(t){return t-Math.min(t,60/this.tempo*this.smoothing)},s.prototype.slide=function(t,e,s){var i=this.getNextNote(t),o=this.getSlideStartDelay(s);return this.setFrequency(this.notes[t].frequency,e+o),this.rampFrequency(i.frequency,e+s),this},s.prototype.setFrequency=function(t,e){return this.osc.frequency.setValueAtTime(t,e),this},s.prototype.rampFrequency=function(t,e){return this.osc.frequency.linearRampToValueAtTime(t,e),this},s.prototype.play=function(t){return t="number"==typeof t?t:this.ac.currentTime,this.createOscillator(),this.osc.start(t),this.notes.forEach(function(e,s){t=this.scheduleNote(s,t)}.bind(this)),this.osc.stop(t),this.osc.onended=this.loop?this.play.bind(this,t):null,this},s.prototype.stop=function(){return this.osc&&(this.osc.onended=null,this.osc.disconnect(),this.osc=null),this},t.Note=e,t.Sequence=s});//FILE: functions.js
 function rnd(min,max){return Math.round(Math.random() * (max - min) + min);}
-function prob(p){return Math.random()*100<=p}
-function dpd() {if(DEBUG) console.log.apply(this, arguments)}
-const dp=console.log
-function $(i) {return document.getElementById(i)}
+function prob(p){return Math.random()*100<=p;}
+function dpd() {if(DEBUG) console.log.apply(this, arguments);}
+const dp=console.log;
+function $(i) {return document.getElementById(i);}
 //FILE: init.js
 var G = {};
 var DEBUG = false;
-G.ui = {}
+G.ui = {};
 G.ui.fps = 20 //50;
 G.ui.width = 150;
 G.ui.height = 150;
 G.ui.scaleX = Math.floor(window.innerWidth/G.ui.width)||1;
 G.ui.scaleY = Math.floor(window.innerHeight/G.ui.height)||1;
 if (G.ui.scaleX>G.ui.scaleY) G.ui.scaleX=G.ui.scaleY; else G.ui.scaleY=G.ui.scaleX;
-G.ui.width=Math.floor(window.innerWidth/G.ui.scaleX)
-G.gravity=.3;
+G.ui.width=Math.floor(window.innerWidth/G.ui.scaleX);
+G.gravity=0.3;
 G.minJump=2;
 G.maxJump=12;
 G.jumpPower=4; 
 G.ui.floor = 8;
 G.ui.horizon = 12;
-G.ui.palette0 = {light:'#EEE', dark:'#333', mid:'#CCC'}
-G.ui.palette1 = {light:'#333', dark:'#EEE', mid:'#CCC'}
-G.ui.palette = G.ui.palette0 
-G.ui.camera = {}
+G.ui.palette0 = {light:'#EEE', dark:'#333', mid:'#CCC'};
+G.ui.palette1 = {light:'#333', dark:'#EEE', mid:'#CCC'};
+G.ui.palette = G.ui.palette0;
+G.ui.camera = {};
 
 G.init = function() {
 	document.body.style.padding=document.body.style.margin='0px';
@@ -40,7 +40,7 @@ G.init = function() {
 	G.ui.area.height=Math.floor(G.ui.height*G.ui.scaleY);
 	G.ui.area.style.top=((window.innerHeight-G.ui.area.height)/3)+'px';
 	G.ui.area.ctx.imageSmoothingEnabled=false;
-	G.ui.pts = {}
+	G.ui.pts = {};
 	G.playerDefault={
 		id: 'player',
 		l: '3',
@@ -66,7 +66,7 @@ G.init = function() {
 	G.ui.terrain.init();
 	if (G.playerDefault.image.complete) G.startMain();
 	else G.playerDefault.image.onload = G.startMain;
-}
+};
 //FILE: sprites.js
 function makeSprite(X,H,a) {
 	var res = [];
@@ -129,7 +129,7 @@ G.ui.speaker.stop=function() {this.speaking=false;};
 G.ui.speaker.speak=function() {
 	var that = G.ui.speaker;
 	if (!that.speaking) return;
-	G.ui.area.ctx.drawImage(G.player.image, 22*that.frame, that.h, that.w, that.h, Math.round(G.menu.rectX-that.w*G.ui.scaleX/1.5), Math.round(G.menu.rectY-that.h*G.ui.scaleY/1.3), that.w*G.ui.scaleX, that.h*G.ui.scaleY)
+	G.ui.area.ctx.drawImage(G.player.image, 22*that.frame, that.h, that.w, that.h, Math.round(G.menu.rectX-that.w*G.ui.scaleX/1.5), Math.round(G.menu.rectY-that.h*G.ui.scaleY/1.3), that.w*G.ui.scaleX, that.h*G.ui.scaleY);
 	if (that.frame<1)that.frame++;else that.frame=0;
 	//G.ui.area.ctx.imageSmoothingEnabled=true;
 	window.setTimeout(that.speak, 300);
